51f6109cc1e4fc660f750ec80c2c44b7309e9fb3
Your Name <you@example.com>
2024-10-18 00:22:01 +0300

    add meanshift_cpu

---
+++ gamac/src/algorithms/meanshift_cpu.py
@@ -0,0 +1,38 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class MeanShift:
+    def __init__(self, radius=4):
+        self.radius = radius
+        self.centroids = {}
+
+    def fit(self, data):
+        centroids = {i: data[i] for i in range(len(data))}
+        optimized = False
+
+        while not optimized:
+            new_centroids = []
+            for i in centroids:
+                in_bandwidth = []
+                centroid = centroids[i]
+                for featureset in data:
+                    if np.linalg.norm(featureset - centroid) < self.radius:
+                        in_bandwidth.append(featureset)
+                new_centroid = np.mean(in_bandwidth, axis=0)
+                new_centroids.append(tuple(new_centroid))
+            uniques = sorted(list(set(new_centroids)))
+            optimized = len(uniques) == len(centroids)
+            centroids = {i: uniques[i] for i in range(len(uniques))}
+
+        self.centroids = centroids
+
+
+# # Example
+# X = np.array([[1, 2], [1.5, 1.8], [5, 8], [8, 8], [1, 0.6], [9, 11], [8, 2], [10, 2], [9, 3]])
+# mean_shift = MeanShift()
+# mean_shift.fit(X)
+# plt.scatter(X[:, 0], X[:, 1], s=150)
+# for c in mean_shift.centroids.values():
+#     plt.scatter(c[0], c[1], color='k', marker='*', s=150)
+# plt.show()
