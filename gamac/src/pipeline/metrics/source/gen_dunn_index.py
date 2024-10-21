3426454e128dc63a8683a80422ffa78744a0cb29
IcyAltair <muratov.syu@edu.spbstu.ru>
2024-10-21 09:46:04 +0300

    #39278864 added dunn, gen dunn, s_dbw

---
+++ gamac/src/pipeline/metrics/source/gen_dunn_index.py
@@ -0,0 +1,16 @@
+from numba import cuda, types
+import numpy as np
+
+from ..utils.funcs import delta3, sigma3
+
+@cuda.jit(())
+def dunn(k_list):
+    s = np.ones([len(k_list), len(k_list)])
+    d = np.zeros([len(k_list), 1])
+    l_range = list(range(0, len(k_list)))
+    for k in l_range:
+        for l in (l_range[0:k] + l_range[k + 1:]):
+            s[k, l] = sigma3(k_list[k], k_list[l])
+            d[k] = delta3(k_list[k])
+            di = np.min(s) / np.max(d)
+    return di
--- gamac/src/pipeline/metrics/utils/funcs.py
+++ gamac/src/pipeline/metrics/utils/funcs.py
@@ -1,6 +1,43 @@
 from numba import cuda
 import numpy as np
 
+
+@cuda.jit(device=True)
+def sigma(ck, cl):
+    values = np.ones([len(ck), len(cl)])
+    for i in range(0, len(ck)):
+        for j in range(0, len(cl)):
+            values[i, j] = np.linalg.norm(ck[i] - cl[j])
+    return np.min(values)
+
+
+@cuda.jit(device=True)
+def sigma3(ck, cl):
+    values = np.ones([len(ck), len(cl)])
+    for i in range(0, len(ck)):
+        for j in range(0, len(cl)):
+            values[i, j] = np.linalg.norm(ck[i] - cl[j])
+    return values / (len(ck) * len(cl))
+
+
+@cuda.jit(device=True)
+def delta(ci):
+    values = np.zeros([len(ci), len(ci)])
+    for i in range(0, len(ci)):
+        for j in range(0, len(ci)):
+            values[i, j] = np.linalg.norm(ci[i] - ci[j])
+    return np.max(values)
+
+
+@cuda.jit(device=True)
+def delta3(ci):
+    values = np.zeros([len(ci), len(ci)])
+    for i in range(0, len(ci)):
+        for j in range(0, len(ci)):
+            values[i, j] = np.linalg.norm(ci[i] - ci[j])
+    return 2 * values / (len(ci))
+
+
 @cuda.jit(device=True)
 def euclidean_distances_numba(X, Y=None, Y_norm_squared=None):
     XX_ = (X * X).sum(axis=1)
@@ -12,7 +49,7 @@ def euclidean_distances_numba(X, Y=None, Y_norm_squared=None):
         YY = Y_norm_squared
     else:
         YY_ = np.sum(Y * Y, axis=1)
-        YY = YY_.reshape((1,-1))
+        YY = YY_.reshape((1, -1))
 
     distances = np.dot(X, Y.T)
     distances *= -2
@@ -21,6 +58,7 @@ def euclidean_distances_numba(X, Y=None, Y_norm_squared=None):
     distances = np.maximum(distances, 0)
 
     return np.sqrt(distances)
+
 
 @cuda.jit(device=True)
 def euclidean_distances_sum(X, Y=None):
@@ -47,9 +85,10 @@ def euclidean_distances_mean(X, Y=None):
 
     return means
 
+
 @cuda.jit(device=True)
 def filter_list(a, b):
     c = []
     for x in a:
-        c.append(x==b)
+        c.append(x == b)
     return c
